a4bdda0cbc9b08cddb6404f0b0470fa3d987810d
YOUR.NAME <hubert69069@o2.pl>
2025-12-27 20:08:57 +0100

    Updated web display

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {})
